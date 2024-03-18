944108bd7c1beca45a4bd50830fd2c8cc99ebe0c
JingyiNiu <niu.jy1203@gmail.com>
2024-03-18 14:07:56 +1300

    end of section 2

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,220 @@
+using API.Entities;
+
+namespace API.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StoreContext context)
+        {
+            if(context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "ABC Speedster Board 2000",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 20000,
+                    PictureUrl = "/images/products/sb-abc-speedster.png",
+                    Brand = "ABC",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green ABC Board 3000",
+                    Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                    Price = 15000,
+                    PictureUrl = "/images/products/sb-abc-green.png",
+                    Brand = "ABC",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "XYZ Board Speed Rush 3",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    PictureUrl = "/images/products/sb-xyz-speed.png",
+                    Brand = "XYZ",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "XYZ Super Board",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 30000,
+                    PictureUrl = "/images/products/sb-xyz-super.png",
+                    Brand = "XYZ",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "OPQ Board Super Whizzy Fast",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 25000,
+                    PictureUrl = "/images/products/sb-opq-super.png",
+                    Brand = "OPQ",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "UVW Entry Board",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 12000,
+                    PictureUrl = "/images/products/sb-uvw-entry.png",
+                    Brand = "UVW",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "XYZ Blue Hat",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1000,
+                    PictureUrl = "/images/products/hat-xyz-blue.png",
+                    Brand = "XYZ",
+                    Type = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green OPQ Woolen Hat",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 8000,
+                    PictureUrl = "/images/products/hat-opq-woolen.png",
+                    Brand = "OPQ",
+                    Type = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Purple OPQ Woolen Hat",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1500,
+                    PictureUrl = "/images/products/hat-opq-woolen.png",
+                    Brand = "OPQ",
+                    Type = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Blue XYZ Gloves",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1800,
+                    PictureUrl = "/images/products/glove-code1.png",
+                    Brand = "XYZ",
+                    Type = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green XYZ Gloves",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1500,
+                    PictureUrl = "/images/products/glove-xyz-green.png",
+                    Brand = "XYZ",
+                    Type = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Purple OPQ Gloves",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1600,
+                    PictureUrl = "/images/products/glove-react1.png",
+                    Brand = "OPQ",
+                    Type = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green OPQ Gloves",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1400,
+                    PictureUrl = "/images/products/glove-opq-green.png",
+                    Brand = "OPQ",
+                    Type = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "LMN Red Boots",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 25000,
+                    PictureUrl = "/images/products/boot-redis1.png",
+                    Brand = "LMN",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "LMN Yellow Boots",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 18999,
+                    PictureUrl = "/images/products/boot-core2.png",
+                    Brand = "LMN",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "XYZ Purple Boots",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 19999,
+                    PictureUrl = "/images/products/boot-xyz-purple.png",
+                    Brand = "XYZ",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "ABC Purple Boots",
+                    Description = "Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
+                    Price = 15000,
+                    PictureUrl = "/images/products/boot-abc-purple.png",
+                    Brand = "ABC",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "ABC Blue Boots",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    PictureUrl = "/images/products/boot-abc-blue.png",
+                    Brand = "ABC",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+            };
+
+        context.Products.AddRange(products);
+        // foreach (var product in products)
+        // {
+        //     context.Products.Add(product);
+        // }
+
+        context.SaveChanges();
+        }
+    }
+}
