cea08ed11faada241f5109018d049f9a1a9c5b78
JingyiNiu <niu.jy1203@gmail.com>
2024-05-21 11:01:32 +1200

    sort pagination in client side

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,23 +2,64 @@ import { useEffect } from "react";
 import ProductList from "./ProductList";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { Grid, Paper } from "@mui/material";
+import ProductSearch from "./ProductSearch";
+import RadioButtonGroup from "../../app/components/RadioGroupButtons";
+import CheckboxButtons from "../../app/components/CheckBoxButtons";
+import AppPagination from "../../app/components/AppPagination";
+
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "priceDesc", label: "Price - High to low" },
+  { value: "price", label: "Price - Low to high" },
+];
 
 const Catalog = () => {
   const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status } = useAppSelector((state) => state.catalog);
+  const { productsLoaded, filtersLoaded, types, brands, productParams, metaData } = useAppSelector(
+    (state) => state.catalog
+  );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!productsLoaded) dispatch(fetchProductsAsync());
   }, [productsLoaded]);
 
-  if (status.includes("pending")) return <LoadingComponent message="Loading products..." />;
+  useEffect(() => {
+    if (!filtersLoaded) dispatch(fetchFilters());
+  }, [dispatch, filtersLoaded]);
+
+  if (!filtersLoaded) return <LoadingComponent message="Loading products..." />;
 
   return (
-    <>
-      <ProductList products={products} />
-    </>
+    <Grid container columnSpacing={4}>
+      <Grid item xs={3}>
+        <Paper sx={{ mb: 2 }}>
+          <ProductSearch />
+        </Paper>
+        <Paper sx={{ p: 2, mb: 2 }}>
+          <RadioButtonGroup
+            selectedValue={productParams.orderBy}
+            options={sortOptions}
+            onChange={(e) => console.log(e.target.value)}
+          />
+        </Paper>
+        <Paper sx={{ p: 2, mb: 2 }}>
+          <CheckboxButtons items={brands} checked={productParams.brands} onChange={(value) => console.log(value)} />
+        </Paper>
+        <Paper sx={{ p: 2 }}>
+          <CheckboxButtons items={types} checked={productParams.types} onChange={(value) => console.log(value)} />
+        </Paper>
+      </Grid>
+      <Grid item xs={9}>
+        <ProductList products={products} />
+      </Grid>
+      <Grid item xs={3} />
+      <Grid item xs={9} sx={{ mb: 2, mt: 2 }}>
+        {metaData && <AppPagination metaData={metaData} onPageChange={(value) => console.log(value)} />}
+      </Grid>
+    </Grid>
   );
 };
 
---
+++ client/src/features/catalog/ProductSearch.tsx
@@ -0,0 +1,7 @@
+import { TextField } from "@mui/material";
+
+const ProductSearch = () => {
+  return <TextField label="Search products" variant="outlined" fullWidth />;
+};
+
+export default ProductSearch;
