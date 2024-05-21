888e6cfe28fc3613474c41c8309e8f3de085eff4
JingyiNiu <niu.jy1203@gmail.com>
2024-05-21 13:58:51 +1200

    set up identity

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -1,12 +1,34 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Data
 {
     public static class DbInitializer
     {
-        public static void Initialize(StoreContext context)
+        public static async Task Initialize(StoreContext context, UserManager<User> userManager)
         {
-            if(context.Products.Any()) return;
+            if (!userManager.Users.Any())
+            {
+                var user = new User
+                {
+                    UserName = "bob",
+                    Email = "bob@test.com"
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Member");
+
+                var admin = new User
+                {
+                    UserName = "admin",
+                    Email = "admin@test.com"
+                };
+
+                await userManager.CreateAsync(admin, "Pa$$w0rd");
+                await userManager.AddToRolesAsync(admin, new[] { "Admin", "Member" });
+            }
+
+            if (context.Products.Any()) return;
 
             var products = new List<Product>
             {
@@ -208,13 +230,13 @@ namespace API.Data
                 },
             };
 
-        context.Products.AddRange(products);
-        // foreach (var product in products)
-        // {
-        //     context.Products.Add(product);
-        // }
+            context.Products.AddRange(products);
+            // foreach (var product in products)
+            // {
+            //     context.Products.Add(product);
+            // }
 
-        context.SaveChanges();
+            context.SaveChanges();
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API.Data;
+using API.Entities;
 using API.Middlewares;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +14,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Connect to MySql
-builder.Services.AddDbContext<StoreContext>(opt => 
+builder.Services.AddDbContext<StoreContext>(opt =>
 {
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
     opt.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), ServerVersion.AutoDetect(connectionString));
@@ -20,6 +22,12 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 
 // Add Cors
 builder.Services.AddCors();
+
+builder.Services.AddIdentityCore<User>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<StoreContext>();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -44,11 +52,12 @@ app.MapControllers();
 
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 try
 {
-    context.Database.Migrate();
-    DbInitializer.Initialize(context);   
+    await context.Database.MigrateAsync();
+    await DbInitializer.Initialize(context, userManager);
 }
 catch (Exception ex)
 {
