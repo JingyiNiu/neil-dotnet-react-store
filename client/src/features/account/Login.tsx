bfaa0b17d6d631ed5af82103aaf9860332f02ac5
JingyiNiu <niu.jy1203@gmail.com>
2024-05-27 16:28:39 +1200

    form validation

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,19 +1,27 @@
 import { LockOutlined } from "@mui/icons-material";
 import { LoadingButton } from "@mui/lab";
 import { Container, Paper, Avatar, Typography, Box, TextField, Grid } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import agent from "../../app/api/agent";
 import { FieldValues, useForm } from "react-hook-form";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
-  } = useForm();
+    formState: { isSubmitting, errors, isValid },
+  } = useForm({ mode: "onTouched" });
 
   const submitForm = async (data: FieldValues) => {
-    await agent.Account.login(data);
+    try {
+      await agent.Account.login(data);
+      navigate(location.state?.from || "/catalog");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -29,16 +37,34 @@ const Login = () => {
         Sign in
       </Typography>
       <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
-        <TextField {...register("email")} margin="normal" fullWidth label="Email" autoComplete="email" autoFocus />
         <TextField
-          {...register("password")}
+          {...register("email", { required: "Email is required", pattern: /^\S+@\S+\.\S+$/i })}
+          error={!!errors.email}
+          helperText={errors?.email?.message as string}
+          margin="normal"
+          fullWidth
+          label="Email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          {...register("password", { required: "Password is required" })}
+          error={!!errors.password}
+          helperText={errors?.password?.message as string}
           margin="normal"
           fullWidth
           label="Password"
           type="password"
           autoComplete="current-password"
         />
-        <LoadingButton loading={isSubmitting} type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        <LoadingButton
+          loading={isSubmitting}
+          disabled={!isValid}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
           Sign In
         </LoadingButton>
         <Grid container>
